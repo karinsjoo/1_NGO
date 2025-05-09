63f4ca969aee832b9a835daf9e9556c98c6a1023
karinsjoo <karinsjoo@hotmail.com>
2025-05-09 12:01:07 +0200

    Initial commit

---
+++ src/sdgswedenprojekt/SDGSwedenprojekt.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sdgswedenprojekt;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+/**
+ *
+ * @author karin
+ */
+public class SDGSwedenprojekt {
+
+    private static InfDB idb;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try
+        {
+            idb = new InfDB("sdgsweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            new Inloggning(idb).setVisible(true);
+            System.out.println("funkar");
+        }catch (InfException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}
